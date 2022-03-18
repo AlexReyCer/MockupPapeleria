ad3ff46254c009980af792b6ff36dd381f3b5038
Zere0 <ale.rey.cer@gmail.com>
2022-03-18 10:36:33 -0600

    Add files via upload

---
+++ contacto.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Papeleria La Ideal</title>
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+
+	<div id="contenedor">
+
+		<header>
+			<div id="logo">
+				<img src="images/logo.png" alt="">
+			</div>
+			<nav id="nav">
+				<ul>
+					<a href="index.php"><li>INICIO</li></a>
+					<a href="escolar.php"><li>ESCOLAR</li></a>
+					<a href="oficina.php"><li>OFICINA</li></a>
+					<a href="pedido.php"><li>PEDIDO</li></a>
+				</ul>
+			</nav>
+		</header>
+		
+
+		<section id="contenido">
+
+		</section>
+
+
+		<aside>
+			<a href="contacto.php"><img src="images/contactanos.png" alt=""></a>
+		</aside>
+		<footer>
+			<figure id="utc">
+				<img src="images/render.png" alt="">
+			</figure>
+			<div id="team">
+				<p>Papeleria La Ideal, Ubicada entre Ocampo y Gonzales Treviño</p>
+				<p>
+					Yadira Amairany Escobedo Soriano <br>
+					Valeria Berenice Navarro Guevara <br>
+					Alejandro Reyes Cerecero <br>
+					Cristian de Jesus Zul Nuñez 
+				</p>
+			</div>
+		</footer>
+
+	</div>
+
+</body>
+</html>
